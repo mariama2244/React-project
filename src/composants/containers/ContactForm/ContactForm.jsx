3f86b7ec2471a1d18c1325430f7bfc31c29c5d36
mariama2244 <119751878+mariama2244@users.noreply.github.com>
2024-12-18 15:36:48 +0100

    cour contactform

--- src/composants/containers/ContactForm/ContactForm.jsx
+++ src/composants/containers/ContactForm/ContactForm.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import Bouton from "../../ui/Bouton/Bouton";
 export default function ContactForm(){
+    //Variable pour stocker les données entrées dans le champs
+    const [fullNameInput, setFullNameInput] = useState('');
+
+    function gereFullName(event){
+        //Recuperer l'entrée de l'utilisateur
+        const valeur = event.target.value; 
+        // Stocker la valeur de l'input dans la variable d'etat
+        setFullNameInput(valeur);
+
+    }
+
+    function submit(){
+        //Recuperer les données du foemulaire
+        alert('Contact' + fullNameInput)
+    }
     return(
-        <div></div>
+        <div className="p-4">
+            <h2>Contact Nous</h2>
+            <input
+            onChange={gereFullName}
+            className="bg-neutral-800 p-2 w-full"
+            type="text"
+            placeholder="Nom et Prenom" />
+            <Bouton gereClique={submit}>Envoyer le message</Bouton>
+        </div>
     )
 }
