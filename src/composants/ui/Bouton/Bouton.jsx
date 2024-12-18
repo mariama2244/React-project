33602173ff9fa229a03ec52feb555da752aceda2
mariama2244 <119751878+mariama2244@users.noreply.github.com>
2024-12-18 09:41:58 +0100

    install tailwind

--- src/composants/ui/Bouton/Bouton.jsx
+++ src/composants/ui/Bouton/Bouton.jsx
@@ -1,21 +1,24 @@
 import PropTypes from 'prop-types'
-export default function Bouton({ couleur, children }) {
-  
-    return (
-        <button
-         style= {{
-            backgroundColor: couleur ? couleur : "green", 
-            color: "white",
-            padding: "0.7em",
-            border: "none",
-            borderRadius: "5px"
-        }}>
-        {children}
-        </button>
-    )
-}
 
 Bouton.propTypes = {
     couleur: PropTypes.string,
     children: PropTypes.node.isRequired
 }
+export default function Bouton({ couleur, children }) {
+  
+    return (
+        <button
+        //  style= {{
+        //     backgroundColor: couleur ? couleur : "green", 
+        //     color: "white",
+        //     padding: "0.7em",
+        //     border: "none",
+        //     borderRadius: "5px"
+        // }}
+        className="b-none p-2 rounded hover:opacity-75 transition"
+        style={{ backgroundColor: couleur ?  couleur : "green" }}>
+        {children}
+        </button>
+    )
+}
+
